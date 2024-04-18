14d46e78b2484e4aa604a3460d8f3dfa23f8fe76
tont1990 <tont1990@gmail.com>
2024-04-18 15:33:01 +0900

    tsconfig path setting, Add Package -D @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5001,
+    host: true,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
