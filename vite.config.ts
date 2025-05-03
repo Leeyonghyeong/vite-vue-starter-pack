09f03f9a39e221ce03b639087d53d12e7e7a5534
tont1990 <tont1990@gmail.com>
2025-05-03 14:59:25 +0900

    tailwindcss 최신 버전 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   server: {
     port: 5001,
     host: true,
